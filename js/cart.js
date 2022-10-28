4a6fd6ee51c41473fe0ba170cb245a27719c59c2
gabolivera17 <gabhen8@gmail.com>
2022-10-28 20:14:46 -0300

    Backup de Entrega 6

--- js/cart.js
+++ js/cart.js
@@ -4,18 +4,18 @@ let cantidadArticulo = 1;
 
 let precioArticulo = 1;
 
-function showCartInfo(url){
+function showCartInfo(url) {
 
-    fetch(url)
-    .then(function(respuesta) {
-    return respuesta.json();
+  fetch(url)
+    .then(function (respuesta) {
+      return respuesta.json();
     })
-    .then(function(datos){
+    .then(function (datos) {
 
-        let carrito = datos.articles;
-        console.log(carrito);
+      let carrito = datos.articles;
+      console.log(carrito);
 
-        for(let articulo of carrito){
+      for (let articulo of carrito) {
 
         cantidadArticulo = articulo.count;
         precioArticulo = articulo.unitCost
@@ -34,15 +34,15 @@ function showCartInfo(url){
             </div>
             <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
               <button class="btn btn-link px-2"
-                onclick="this.parentNode.querySelector('input[type=number]').stepDown(); (cantidadArticulo--); recalcularSubtotal()">
+                onclick="this.parentNode.querySelector('input[type=number]').stepDown(); (cantidadArticulo--); recalcularMontos()">
                 <i class="fas fa-minus"></i>
               </button>
 
-              <input id="form1" min="0" name="quantity" value=${cantidadArticulo} type="number"
+              <input id="form1" min="1" name="quantity" value=${cantidadArticulo} type="number"
                 class="form-control form-control-sm" />
 
               <button class="btn btn-link px-2"
-                onclick="this.parentNode.querySelector('input[type=number]').stepUp(); (cantidadArticulo++); recalcularSubtotal()">
+                onclick="this.parentNode.querySelector('input[type=number]').stepUp(); (cantidadArticulo++); recalcularMontos()">
                 <i class="fas fa-plus"></i>
               </button>
             </div>
@@ -57,17 +57,140 @@ function showCartInfo(url){
         </div>    
         `
         document.getElementById("articulos-carrito").innerHTML = contenidoHTML;
-    }
+        document.getElementById("subtotal-carrito").innerHTML = "USD " + cantidadArticulo * precioArticulo;
+        document.getElementById("costo-de-envio").innerHTML = "USD " + Math.round((cantidadArticulo * precioArticulo) * 0.15);
+        document.getElementById("total-carrito").innerHTML = "USD " + Math.round((cantidadArticulo * precioArticulo) + Math.round((cantidadArticulo * precioArticulo) * 0.15))
+      }
     })
 }
 
 showCartInfo(URL_CARRITO);
 
-function recalcularSubtotal(){
-    if(cantidadArticulo >= 0){
+let envioPremium = document.getElementById("premium");
+let envioExpress = document.getElementById("express");
+let envioStandard = document.getElementById("standard");
+
+let costoEnvio;
+
+function recalcularMontos() {
+
+  if (envioPremium.checked) {
+    costoEnvio = Math.round((cantidadArticulo * precioArticulo) * 0.15);
+    document.getElementById("costo-de-envio").innerHTML = "USD " + costoEnvio;
+  }
+  else if (envioExpress.checked) {
+    costoEnvio = Math.round((cantidadArticulo * precioArticulo) * 0.07);
+    document.getElementById("costo-de-envio").innerHTML = "USD " + costoEnvio;
+  }
+  else {
+    costoEnvio = Math.round((cantidadArticulo * precioArticulo) * 0.05);
+    document.getElementById("costo-de-envio").innerHTML = "USD " + costoEnvio;
+  }
+
+  if (cantidadArticulo >= 1) {
     document.getElementById("subtotal").innerHTML = "USD " + cantidadArticulo * precioArticulo;
-    }
-    else{
-        cantidadArticulo = 0;
-    }
+    document.getElementById("subtotal-carrito").innerHTML = " USD " + cantidadArticulo * precioArticulo;
+  }
+  else {
+    cantidadArticulo = 1;
+  }
+
+  document.getElementById("total-carrito").innerHTML = "USD " + Math.round((cantidadArticulo * precioArticulo) + costoEnvio);
 }
+
+function pagoTarjeta(){
+
+  document.getElementById("form-tarjeta").disabled = false;
+  
+  document.getElementById("form-transferencia-bancaria").disabled = true;
+}
+
+function pagoCuentaBancaria(){
+  
+  document.getElementById("form-tarjeta").disabled = true;
+  
+  document.getElementById("form-transferencia-bancaria").disabled = false;
+}
+
+
+
+
+document.getElementById("boton-pagar").addEventListener("click",function(evento){
+
+  let incompleto = false;
+
+  let inputCalle = document.getElementById("input-calle").value;
+  let inputNumero = document.getElementById("input-numero").value;
+  let inputEsquina = document.getElementById("input-esquina").value;
+
+  document.getElementById("mensaje-calle").innerHTML = "";
+  document.getElementById("mensaje-numero").innerHTML = "";
+  document.getElementById("mensaje-esquina").innerHTML = "";
+  document.getElementById("mensaje-envio").innerHTML = "";
+  document.getElementById("mensaje-pago").innerHTML = "";
+
+  if (inputCalle.length <= 0) {
+    evento.preventDefault();
+    document.getElementById("mensaje-calle").innerHTML = "Debe ingresar una calle";
+    document.getElementById("input-calle").style.border = "1px solid crimson";
+    incompleto = true;
+  }
+  else {
+    document.getElementById("input-calle").style.border = "2px solid green";
+    incompleto = false;
+  }
+
+  if (inputNumero.length <= 0) {
+    evento.preventDefault();
+    document.getElementById("mensaje-numero").innerHTML = "Debe ingresar un número";
+    document.getElementById("input-numero").style.border = "1px solid crimson";
+    incompleto = true;
+  }
+  else{
+    document.getElementById("input-numero").style.border = "2px solid green";
+    incompleto = false;
+  }
+
+  if (inputEsquina.length <= 0) {
+    evento.preventDefault();
+    document.getElementById("mensaje-esquina").innerHTML = "Debe ingresar una esquina";
+    document.getElementById("input-esquina").style.border = "1px solid crimson";
+    incompleto = true;
+  }
+  else{
+    document.getElementById("input-esquina").style.border = "2px solid green";
+    incompleto = false;
+  }
+
+  if (!envioPremium.checked && !envioExpress.checked && !envioStandard.checked){
+    evento.preventDefault();
+    document.getElementById("mensaje-envio").innerHTML = "Debe seleccionar un tipo de envío";
+    document.getElementById("carta-envio").style.border = "1px solid crimson";
+    incompleto = true;
+  }
+  else{
+    document.getElementById("carta-envio").style.border = "2px solid green";
+    incompleto = false;
+  }
+
+  if(!document.getElementById("tarjeta-de-credito").checked && !document.getElementById("transferencia-bancaria").checked){
+    evento.preventDefault();
+    document.getElementById("mensaje-pago").innerHTML = "Debe seleccionar una forma de pago";
+    incompleto = true;
+  }
+  else{
+    incompleto = false;
+  }
+
+  if(incompleto){
+    document.getElementById("mensaje-compra").innerHTML = "Por favor completar campos para continuar";
+    document.getElementById("mensaje-compra").style.color = "crimson";
+  }
+  else{
+    alert(`Su compra se realizó correctamente.
+Presione aceptar para volver al inicio`);
+
+    window.location.href = "index2.html"
+  }
+})
+
